8036042a69387d6fad0f36032055ad4eb87dc182
KennyKvn001 <k.mugisha@alustudent.com>
2024-08-02 01:48:16 +0200

    Connect new DB

--- MenyaDigTest/MenyaDigTest/settings.py
+++ MenyaDigTest/MenyaDigTest/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,6 +93,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DATABASES["default"] = dj_database_url.config(default=os.getenv("DATABASE_URL"))
 
 
 # Password validation
